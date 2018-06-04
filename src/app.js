6eecb311667c0c6ee0928874e4701bfea7aba963
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-04 00:56:10 -0300

    Configurações finais do servidor

--- src/app.js
+++ src/app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 
 const route = router.get('/', (req, res, next) => {
-
     res.status(200).send({
         title: "Node Store API",
-        version: "0.0.1"
+        version: "0222.0.2"
     });
-
 });
 
 app.use('/', route);
