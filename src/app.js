cd35f8745672d12d1e64248631d37907d500ee0e
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-04 01:20:20 -0300

    criando uma rota e instalando o bodyparser

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -9,6 +14,11 @@ const route = router.get('/', (req, res, next) => {
     });
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
 app.use('/', route);
+app.use('/products', create);
 
 module.exports = app;
