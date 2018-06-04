698ef168b11ffc3ef6281f10bb2a7d18f376fba0
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-03 21:25:46 -0300

    tratando erros

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const debug = require('debug')('cursonode:server')
 const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 
 app.set('port', port);
 
@@ -25,4 +25,43 @@ app.use('/', route);
 
 server.listen(port);
 console.log('API rodando na porta ' + port);
+
+function normalizePort(val){
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)){
+        return val;
+    }
+
+    if(port >= 0){
+        return port;
+    }
+
+    return false;
+}
+
+function onError(error){
+    if(error.syscall !== 'listen'){
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+    
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + 'requires elevated previleges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already is use');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+
+    }
+}
+
 
