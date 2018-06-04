d0bffcb2c2883a225b3b436a36ec90cf7b7d8350
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-06-04 01:28:10 -0300

    Rota com método put

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -18,7 +18,16 @@ const create = router.post('/', (req, res, next) => {
     res.status(201).send(req.body);
 });
 
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(201).send({
+        id: id,
+        item: req.body
+    });
+});
+
 app.use('/', route);
 app.use('/products', create);
+app.use('/products', put);
 
 module.exports = app;
